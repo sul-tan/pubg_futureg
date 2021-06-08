9b9a24b26e87780e683bd2d8918018327b6e1b44
sul-tan <65980224+sul-tan@users.noreply.github.com>
2021-06-08 13:20:01 +0530

    Update pubg.py

--- pubg.py
+++ pubg.py
@@ -4,10 +4,10 @@ app = Flask(__name__)
 @app.route('/')
 def main():
     return render_template("index.html")
-@app.route('/fee', methods=["POST"])   
+@app.route('/fee', methods=["GET","POST"])   
 def fee():
     return render_template("fee.html") 
-@app.route('/details',methods=["post"])  
+@app.route('/details',methods=["GET","post"])  
 def details():
     return render_template("form.html")  
 if __name__ == "__main__":
